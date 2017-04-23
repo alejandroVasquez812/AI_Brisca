f1246d9bbe71e69e2b2cfe602f4b88f0d93335ef
Alejandro Vasquez <alejandro.vasquez@upr.edu>
2017-04-23 13:57:37 -0400

    Prob
    Adding some things to create probabilities

--- com/ai/brisca_agent/Agent.java
+++ com/ai/brisca_agent/Agent.java
@@ -3,16 +3,35 @@ package com.ai.brisca_agent;
 import com.ai.brisca_agent.entities.Card;
 import sun.reflect.generics.tree.Tree;
 
+import javax.swing.tree.DefaultTreeModel;
 import javax.swing.tree.TreeNode;
+import java.util.ArrayList;
+import java.util.Enumeration;
 
 /**
  *
  */
 public class Agent {
-    public Card selectedPlay(String triunfo, Card[] myCards, Card[] tableCards) {
+    private ArrayList<Card> myCards;
+    private String triunfo;
+    private ArrayList<Card> bastosJugados;
+    private ArrayList<Card> copasJugadas;
+    private ArrayList<Card> oroJugados;
+    private ArrayList<Card> espadasJugadas;
+    /*
+    Create the agent with the first cards
+     */
+    public Agent(Card[] myHand,String triunfo){
+        for(int i=0;i<=2;i++)
+            myCards.add(myHand[i]);
+        this.triunfo=triunfo;
+    }
+    /*
+    Receive the cards in the table and select the best play
+     */
+    public Card playTurn(Card[] tableCards) {
         //TODO
-
-
+        updateCards(tableCards);
         Card selectedCard=null;
         return selectedCard ;
 
@@ -24,7 +43,27 @@ public class Agent {
     public void minValue(){
         //TODO
     }
+    /*
+    Adds the cards to the corresponding list by type
+     */
+    public void updateCards(Card[] cards){
+        for (Card i:tableCards){
+            switch (i.getSuit()){
+                case "basto": bastosJugados.add(i);
+                break;
+                case "copa": copasJugadas.add(i);
+                break;
+                case "oro": oroJugados.add(i);
+                break;
+                case "espadas": espadasJugadas.add(i);
+                break;
+            }
+
+        }
+    }
     private void createTree(){
+
+
 
     }
 }
